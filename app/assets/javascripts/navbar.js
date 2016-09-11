c2f891f0baa0345174a8a4e009055be1d0a47329
GrumpyGrandpa <budakristi@gmail.com>
2016-09-11 16:53:50 +0200

    TEST

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,34 +1,37 @@
-function getScreenSize() {
-  var width = parseInt($(window).width());
-
-  if(width <= 800){
-    $('#buda-navbar-jumbotron').fadeIn("slow");
-    $('.buda-navbar-follower').fadeOut("slow");
-  }
-  else if(width > 800){
-    $('#buda-navbar-jumbotron').fadeIn("slow");
-    $('.buda-navbar-follower').fadeOut("slow");
-  }
-};
-$(document).ready(getScreenSize);
-$(window).resize(getScreenSize);
+// function getScreenSize() {
+//   var width = parseInt($(window).width());
+//
+//   if(width <= 800){
+//     $('#buda-navbar-jumbotron').fadeIn("slow");
+//     $('.buda-navbar-follower').fadeOut("slow");
+//   }
+//   else if(width > 800){
+//     $('#buda-navbar-jumbotron').fadeIn("slow");
+//     $('.buda-navbar-follower').fadeOut("slow");
+//   }
+// };
+// $(document).ready(getScreenSize);
+// $(window).resize(getScreenSize);
 
 
 $(document).ready(function() {
+  var width = parseInt($(window).width());
+  $('.buda-navbar-follower').hide();
+
   $(window).scroll(function () {
       console.log($(window).scrollTop())
     if ($(window).scrollTop() > 0) {
       $('.navbar').addClass('navbar-fixed-top');
     }
-    if ($(window).scrollTop() < 0) {
-      $('.navbar').removeClass('navbar-fixed-top');
-    }
+    // if ($(window).scrollTop() < 0) {
+    //   $('.navbar').removeClass('navbar-fixed-top');
+    // }
     if($(window).scrollTop() >= 100){
-      $('#buda-navbar-jumbotron').fadeOut("slow");
+      // $('#buda-navbar-jumbotron').fadeOut("slow");
       $('.buda-navbar-follower').fadeIn("slow");
     }
     else {
-      $('#buda-navbar-jumbotron').fadeIn("slow");
+      // $('#buda-navbar-jumbotron').fadeIn("slow");
       $('.buda-navbar-follower').fadeOut("slow");
     }
   });
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,5 @@
+$("#chevron-down-wallapaper").click(function() {
+    var offset = 20;
+    $('html, body').animate({
+        scrollTop: $("#buda-about").offset().top}, 2000);
+});
