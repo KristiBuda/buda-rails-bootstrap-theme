638f1a087519056f849e3ad2940b02f211c11cc2
GrumpyGrandpa <budakristi@gmail.com>
2016-09-11 13:19:21 +0200

    navbar complete

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,13 +2,34 @@ function getScreenSize() {
   var width = parseInt($(window).width());
 
   if(width <= 800){
-    $('#buda-navbar-jumbotron').fadeOut();
-    $('.buda-navbar-follower').fadeIn();
+    // $('#buda-navbar-jumbotron').fadeOut("slow");
+    // $('.buda-navbar-follower').fadeIn("slow");
   }
   else if(width > 800){
-    $('#buda-navbar-jumbotron').fadeIn();
-    $('.buda-navbar-follower').fadeOut();
+    $('#buda-navbar-jumbotron').fadeIn("slow");
+    $('.buda-navbar-follower').fadeOut("slow");
   }
 };
 $(document).ready(getScreenSize);
 $(window).resize(getScreenSize);
+
+
+$(document).ready(function() {
+  $(window).scroll(function () {
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 0) {
+      $('.navbar').addClass('navbar-fixed-top');
+    }
+    if ($(window).scrollTop() < 0) {
+      $('.navbar').removeClass('navbar-fixed-top');
+    }
+    if($(window).scrollTop() >= 150){
+      $('#buda-navbar-jumbotron').fadeOut("slow");
+      $('.buda-navbar-follower').fadeIn("slow");
+    }
+    else {
+      $('#buda-navbar-jumbotron').fadeIn("slow");
+      $('.buda-navbar-follower').fadeOut("slow");
+    }
+  });
+});
